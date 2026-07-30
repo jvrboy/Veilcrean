6b49e9a2bf25c625b62661d6db97f3c4efd11c94
jvrboy <justinsimpsad@gmail.com>
2026-07-30 23:41:23 +0200

    Upgrade neurosense neurons modules to v1.0.0

--- neurosense/neurosense/neurons/__init__.py
+++ neurosense/neurosense/neurons/__init__.py
@@ -1,33 +1,25 @@
-"""Neural primitives."""
-from .activations import Activation, get_activation, leaky_relu, linear, relu, sigmoid, softmax, tanh
-from .layers import DenseLayer, DropoutLayer, LayerNorm
-from .network import NeuralNetwork, cross_entropy_loss, mse_loss
-from .optimizers import Adam, SGD
-from .spiking import LIFNeuron, SpikingNetwork, poisson_encode
-from .hebbian import HebbianNetwork, HebbianSynapse, hebbian_update, oja_update
+"""Neurons — from-scratch neural computation. No frameworks, only numpy."""
+
+from .network import NeuralNetwork
+from .layers import Dense, Activation
+from .activations import relu, sigmoid, tanh, softmax, ACTIVATIONS
+from .optimizers import SGD, Momentum, Adam
+from .hebbian import HebbianLayer
+from .spiking import SpikingNeuron, SpikingNetwork
 
 __all__ = [
+    "NeuralNetwork",
+    "Dense",
     "Activation",
+    "relu",
     "sigmoid",
     "tanh",
-    "relu",
-    "leaky_relu",
-    "linear",
     "softmax",
-    "get_activation",
-    "DenseLayer",
-    "DropoutLayer",
-    "LayerNorm",
-    "NeuralNetwork",
-    "mse_loss",
-    "cross_entropy_loss",
+    "ACTIVATIONS",
     "SGD",
+    "Momentum",
     "Adam",
-    "LIFNeuron",
+    "HebbianLayer",
+    "SpikingNeuron",
     "SpikingNetwork",
-    "poisson_encode",
-    "HebbianSynapse",
-    "HebbianNetwork",
-    "hebbian_update",
-    "oja_update",
 ]
